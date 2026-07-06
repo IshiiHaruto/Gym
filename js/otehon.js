ecb5fad9ebe1349c41e2fc2d1c4b24c4196a2daf
Limiumu <start.star14rime@gmail.com>
2026-07-06 15:13:07 +0900

    qwert

--- js/otehon.js
+++ js/otehon.js
@@ -20,3 +20,11 @@ $(function () {
         500);
     });
 });
+
+// 左からニョキニョキ
+const button = document.querySelector(".toggle-menu-button");
+const menu = document.querySelector(".header-site-menu");
+
+button.addEventListener("click", () => {
+    menu.classList.toggle("open");
+});
