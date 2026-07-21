f5bd9bf04d572bae124e325350414e57c11708af
Tatsuki Inoue <235835427+251205021-oss@users.noreply.github.com>
2026-07-21 14:52:44 +0900

    昨日分

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
-// 左からニョキニョキ
-const button = document.querySelector(".toggle-menu-button");
-const menu = document.querySelector(".header-site-menu");
+document.addEventListener("DOMContentLoaded", function () {
 
-button.addEventListener("click", () => {
-    menu.classList.toggle("open");
+    const button = document.querySelector(".toggle-menu-button");
+    const menu = document.querySelector(".header-site-menu");
+
+    console.log(button);
+    console.log(menu);
+
+    button.addEventListener("click", function () {
+        console.log("クリックされた");
+        menu.classList.toggle("open");
+    });
+
 });
